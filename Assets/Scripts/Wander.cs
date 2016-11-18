2274e8699822aeb98388b2186f5298d49b055f06
John Park (RIT Student) <jrp3024@ad.rit.edu>
2016-11-17 20:46:57 -0500

    Added wandering

---
+++ Assets/Scripts/Wander.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Wander : MonoBehaviour {
+
+    NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = GetRandomPoint();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(agent.remainingDistance < 0.5f)
+        {
+            agent.destination = GetRandomPoint();
+        }
+	}
+
+    Vector3 GetRandomPoint()
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * 10.0f;
+        randomDirection += transform.position;
+        NavMeshHit hit;
+        NavMesh.SamplePosition(randomDirection, out hit, 10.0f, 1);
+        return hit.position;
+    }
+}
