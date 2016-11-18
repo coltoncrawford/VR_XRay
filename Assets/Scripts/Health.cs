f018381d7adca008f372412cb3f4ea26765eb3c3
Alec Linder (RIT Student) <afl4947@ad.rit.edu>
2016-11-17 19:55:32 -0500

    Elephants now carry things around, and swords.

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,17 +5,32 @@ public class Health : MonoBehaviour {
 
     public float health = 100;
 
+    public Animator anim;
+    public HiddenObjects obs;
+
+    private bool isDead = false;
+
 	// Use this for initialization
 	void Start () {
-	
-	}
+        
+    }
 	
     public void Damage(float a)
     {
+        if (isDead) return;
         health -= a;
         if(health < 0)
         {
-            Destroy(gameObject);
+            isDead = true;
+            if(anim != null)
+            {
+                anim.SetBool("isDead", true);
+            }
+            if(obs != null)
+            {
+                obs.HideHiddenObjects();
+            }
+            //Destroy(gameObject);
         }
     }
 }
