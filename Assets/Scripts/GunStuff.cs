902e96daab43656973e9f7849ed5fd06a4432993
John Park (RIT Student) <jrp3024@ad.rit.edu>
2016-11-17 19:56:42 -0500

    stuff

--- Assets/Scripts/GunStuff.cs
+++ Assets/Scripts/GunStuff.cs
@@ -5,10 +5,11 @@ public class GunStuff : MonoBehaviour {
 
     public SteamVR_Controller.DeviceRelation controller;
 
-    public GameObject firePoint;
     public GameObject bullet;
 
     private float bulletTimer = 0;
+
+    public int ammo;
 
 	// Use this for initialization
 	void Start () {
@@ -18,13 +19,13 @@ public class GunStuff : MonoBehaviour {
         
         if(bulletTimer >= 0.1f)
         {
-            Rigidbody rb = ((GameObject)Instantiate(bullet, firePoint.transform.position, Quaternion.identity)).GetComponent<Rigidbody>();
-            rb.AddForce((firePoint.transform.forward + Random.insideUnitSphere * 0.01f).normalized * 10000);
+            Rigidbody rb = ((GameObject)Instantiate(bullet, transform.position + (transform.forward), Quaternion.identity)).GetComponent<Rigidbody>();
+            rb.AddForce((transform.forward + Random.insideUnitSphere * 0.01f).normalized * 10000);
             Destroy(rb.gameObject, 10.0f);
 
             bulletTimer = 0;
+            ammo--;
         }
-
     }
 
 	// Update is called once per frame
@@ -32,7 +33,7 @@ public class GunStuff : MonoBehaviour {
         bulletTimer += Time.deltaTime;
 
         if (SteamVR_Controller.Input(SteamVR_Controller.GetDeviceIndex(controller)).GetHairTrigger()) {
-            FireBullet();
+            if(ammo > 0) { FireBullet(); }
         }
 	}
 }
---
+++ Assets/Scripts/HighLightOnPoint.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HighLightOnPoint : MonoBehaviour {
+
+    public Material onHighlight;
+    public Material onBase;
+    public float angleThreshold;
+
+    GameObject[] people;
+
+	// Use this for initialization
+	void Start () {
+        people = GameObject.FindGameObjectsWithTag("person");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    foreach(var person in people)
+        {
+            Vector3 temp = person.transform.position - transform.position;
+            temp.Normalize();
+            if(Vector3.Dot(transform.forward,temp)> angleThreshold)
+            {
+                person.GetComponent<Renderer>().material = onHighlight;
+            } else
+            {
+                person.GetComponent<Renderer>().material = onBase;
+            }
+        }
+	}
+}
