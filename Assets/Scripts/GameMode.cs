802c924565762258fdb917c8b1193b3349c9adca
Alec Linder (RIT Student) <afl4947@ad.rit.edu>
2016-11-17 20:00:42 -0500

    Working on the game mode.

--- Assets/Scripts/GameMode.cs
+++ Assets/Scripts/GameMode.cs
@@ -1,14 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GameMode : MonoBehaviour {
 
     public Transform swordSpawner;
+    public Text stateText;
 
+    private GunStuff gun;
     private Health health;
 
+
+    private bool hasEnded = false;
 	// Use this for initialization
 	void Start () {
+        gun = FindObjectOfType<GunStuff>();
+
         SpawnObjects[] obs = FindObjectsOfType<SpawnObjects>();
         int randomIndex = Random.Range(0, obs.Length);
         Transform sword = Instantiate(swordSpawner);
@@ -18,6 +25,12 @@ public class GameMode : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    if()
 	}
+
+    IEnumerator CheckForEnd()
+    {
+        yield return new WaitForSeconds(0.2f);
+
+    }
 }
